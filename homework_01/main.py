0951fd3da16431c1b9a5242bf88d606323d09079
kycok <konstantinkrivonogov@gmail.com>
2023-03-23 20:52:48 +0400

    Добавлена проверка, что число больше 1

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,39 +1,9 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-
-
 def power_numbers(*number):
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
     square: list[Any] = []
     for el in number:
         square.append(el**2)
     return square
 
-
-# filter types
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
-
-
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
 # filter types
 ODD = "odd"
 EVEN = "even"
@@ -46,7 +16,7 @@ def is_prime(num):
     for i in range(2, num):
         if 0 == num % i:
             k = k + 1
-    if k <= 0 and num != 1:
+    if k <= 0 and 1 < num:
         return True
     else:
         return False
@@ -70,9 +40,9 @@ def filter_even_num(num):
 
 def filter_numbers(num, filter_type):
     if filter_type == ODD:
-        return filter(filter_odd_num, num)
+        return list(filter(filter_odd_num, num))
     elif filter_type == EVEN:
-        return filter(filter_even_num, num)
+        return list(filter(filter_even_num, num))
     elif filter_type == PRIME:
-        return filter(is_prime, num)
+        return list(filter(is_prime, num))
 
