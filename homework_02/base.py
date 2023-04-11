c4153f77c5f51e48bd7cead33425f9036aa06547
kycok <konstantinkrivonogov@gmail.com>
2023-04-12 02:19:33 +0400

    добавлен метод move

--- homework_02/base.py
+++ homework_02/base.py
@@ -9,8 +9,9 @@ class Vehicle(ABC):
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
 
-    def get_fuel(self, fuel):
-        if fuel== 0:
+    @staticmethod
+    def get_fuel(fuel):
+        if fuel == 0:
             return False
         else:
             return True
@@ -20,18 +21,22 @@ class Vehicle(ABC):
             print(started)
         else:
             if self.get_fuel(self.fuel) == False:
-                print('exceptions.LowFuelError') # TO DO переделать в исключение
+                print('exceptions.LowFuelError')  # TO DO переделать в исключение
             else:
                 self.started = 'started'
                 print(started)
 
+    def move(self, distance, fuel, fuel_consumption):
+        if fuel * 100 / fuel_consumption >= distance:
+            self.fuel = fuel - distance * fuel_consumption / 100
+            print(self.fuel)
+        else:
+            print('exceptions.NotEnoughFuel')  # TO DO переделать в исключение
 
-auto_1 = Vehicle(25, 'nostarted', 1, 10)
+
+auto_1 = Vehicle(25, 'nostarted', 20, 10)
 auto_1.start(auto_1.started)
-print(vars(auto_1))
-#print(vars(auto_1))
-#print(auto_1.started)
-
-
-
-
+auto_1.move(100, auto_1.fuel, auto_1.fuel_consumption)
+# print(vars(auto_1))
+# print(vars(auto_1))
+# print(auto_1.started)
