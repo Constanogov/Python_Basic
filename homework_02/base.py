7f868992c64c5758441bd08e55ae074b59a019ec
kycok <konstantinkrivonogov@gmail.com>
2023-05-12 16:13:40 +0400

    10 pass

--- homework_02/base.py
+++ homework_02/base.py
@@ -4,40 +4,44 @@ from homework_02 import exceptions
 
 
 class Vehicle(ABC):
+    started: bool = False
+    distance: int = 0
 
-    def __init__(self, weight, started, fuel, fuel_consumption):
+    def __init__(self, weight, fuel, fuel_consumption):
         self.weight = weight
-        self.started = started
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
 
-    @staticmethod
-    def get_fuel(fuel):
-        if fuel == 0:
-            return False
+    def start(self):
+        if self.started == True:
+            pass
+        elif self.fuel == 0:
+            raise exceptions.LowFuelError
         else:
-            return True
+            self.started = True
 
-    def start(self, started):
-        if started == 'started':
-#            pass
-            print('started test')
-        else:
-            if not self.get_fuel(self.fuel):
-                raise exceptions.LowFuelError
-            else:
-                started = 'started'
-                print(started)
-
-    def move(self, distance, fuel, fuel_consumption):
-        if fuel * 100 / fuel_consumption >= distance:
-            self.fuel = fuel - distance * fuel_consumption / 100
-            print(self.fuel)
+    def move(self, distance):
+        max_distance = self.fuel // self.fuel_consumption
+        fuel_need = self.distance * self.fuel_consumption
+        if self.fuel == 0 or \
+                self.fuel_consumption > self.fuel or \
+                self.distance > max_distance or \
+                fuel_need > self.fuel:
+            raise exceptions.NotEnoughFuel
+        elif fuel_need == self.fuel:
+            self.fuel = self.fuel - self.distance * self.fuel_consumption
         else:
             raise exceptions.NotEnoughFuel
 
 
-auto = Vehicle(1, '', 2, 5)
-#car.fuel = 0
-#print(vars(car))
-auto.start('started')
+
+#auto = Vehicle(1, 0, 5)
+# auto.fuel = 10
+# print(vars(auto))
+#auto.started = True
+#auto.start(auto.started)
+#print(auto.started)
+# auto.started = '1'
+# print(auto.started)
+#print(Vehicle.started)
+#print(Vehicle.start)
--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,18 @@
-"""
-создайте класс `Car`, наследник `Vehicle`
-"""
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+
+    def __init__(self, weight,  fuel, fuel_consumption, engine):
+        super().__init__(weight,  fuel, fuel_consumption)
+        self.engine = engine
+
+    def set_engine(self):
+        return Engine()
+
+#auto = Car(1, '', 1, 1, 1)
+#auto.engine = auto.set_engine()
+#car.fuel = 0
+#print(vars(car))
+#print(vars(auto))
