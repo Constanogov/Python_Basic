a755016739e2db13df423f51a7fe7ed40ad03cf0
kycok <konstantinkrivonogov@gmail.com>
2023-05-15 23:18:54 +0400

    15 pass

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,18 +1,13 @@
+from homework_02 import engine
 from homework_02.base import Vehicle
 from homework_02.engine import Engine
 
 
 class Car(Vehicle):
 
-    def __init__(self, weight,  fuel, fuel_consumption, engine):
-        super().__init__(weight,  fuel, fuel_consumption)
+    def __init__(self, weight, fuel, fuel_consumption, *args, **kwargs):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = Engine(volume=3.5, pistons=8)
+
+    def set_engine(self, engine):
         self.engine = engine
-
-    def set_engine(self):
-        return Engine()
-
-#auto = Car(1, '', 1, 1, 1)
-#auto.engine = auto.set_engine()
-#car.fuel = 0
-#print(vars(car))
-#print(vars(auto))
